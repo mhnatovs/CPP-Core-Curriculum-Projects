b29f1b045489f65060502469e5ed47fd0141bee6
Marianna Hnatovska <mhnatovs@3-A-4.42heilbronn.de>
2026-02-19 13:25:46 +0100

    ex00

---
+++ CPP00/ex00/megaphone.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cctype>
+
+int main(int ac, char **av)
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	if (ac == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+	while (av[i])
+	{
+		j = 0;
+		while (av[i][j])
+		{
+			std::cout << (char)std::toupper(av[i][j]);
+			j++;
+		}
+		i++;
+	}
+	std::cout <<std::endl;
+	return (0);
+}
