98a21e4438ace4cb9fcd1be5982cded32fa627c3
Marianna Hnatovska <mhnatovs@1-A-31.42heilbronn.de>
2026-05-31 14:49:54 +0200

    started cpp05

---
+++ CPP05/Bureaucrat.cpp
@@ -0,0 +1,13 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhnatovs <mhnatovs@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/31 14:12:42 by mhnatovs          #+#    #+#             */
+/*   Updated: 2026/05/31 14:12:43 by mhnatovs         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// either a Bureaucrat::GradeTooHighException or a Bureaucrat::GradeTooLowException
---
+++ CPP05/Bureaucrat.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhnatovs <mhnatovs@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/31 14:12:39 by mhnatovs          #+#    #+#             */
+/*   Updated: 2026/05/31 14:14:01 by mhnatovs         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+# include <iomanip>
+# include <string>
+
+class Bureaucrat {
+    private:
+        const std::string name;
+        int grade;
+        //  («) operator
+
+    public:
+    // getName
+    // getGrade
+
+};
+
+#endif
