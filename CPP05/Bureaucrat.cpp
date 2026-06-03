483d1d8be4ab4d211dc7a8f389bad00679a9ef9b
Marianna Hnatovska <mhnatovs@2-A-1.42heilbronn.de>
2026-06-03 18:00:02 +0200

    .hpp

--- CPP05/Bureaucrat.cpp
+++ CPP05/Bureaucrat.cpp
@@ -6,8 +6,46 @@
 /*   By: mhnatovs <mhnatovs@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/31 14:12:42 by mhnatovs          #+#    #+#             */
-/*   Updated: 2026/05/31 14:12:43 by mhnatovs         ###   ########.fr       */
+/*   Updated: 2026/06/03 17:59:21 by mhnatovs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// either a Bureaucrat::GradeTooHighException or a Bureaucrat::GradeTooLowException
+#include "Bureaucrat.hpp"
+
+/* OCF */
+Bureaucrat::Bureaucrat() {}
+
+Bureaucrat::Bureaucrat(const std::string& name, int grade)
+    : _name(name)
+{
+    
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat& other)
+    : _name(other._name)
+{
+    
+}
+
+Bureaucrat& Bureaucrat::operator=(const Bureaucrat& other)
+{
+    
+}
+
+Bureaucrat::~Bureaucrat() {}
+
+/* member functions */
+const std::string&  Bureaucrat::getName() const
+{
+    return _name;
+}
+
+int Bureaucrat::getGrade() const
+{
+    return _grade;
+}
+
+void incrementGrade();
+void decrementGrade();
+
+/* functions */
--- CPP05/Bureaucrat.hpp
+++ CPP05/Bureaucrat.hpp
@@ -6,26 +6,47 @@
 /*   By: mhnatovs <mhnatovs@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/31 14:12:39 by mhnatovs          #+#    #+#             */
-/*   Updated: 2026/05/31 14:14:01 by mhnatovs         ###   ########.fr       */
+/*   Updated: 2026/06/03 17:59:29 by mhnatovs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BUREAUCRAT_HPP
 # define BUREAUCRAT_HPP
 
-# include <iomanip>
+# include <iostream>
 # include <string>
+# include <stdexcept>
 
 class Bureaucrat {
     private:
-        const std::string name;
-        int grade;
-        //  («) operator
+        const std::string   _name;
+        int        _grade;
 
     public:
-    // getName
-    // getGrade
+        Bureaucrat();
+        Bureaucrat(const std::string& name, int grade);
+        Bureaucrat(const Bureaucrat& other);
+        Bureaucrat& operator=(const Bureaucrat& other);
+        ~Bureaucrat();
+
+        const std::string&  getName() const;
+        int getGrade() const;
+        void incrementGrade();
+        void decrementGrade();
+
+    class GradeTooHighException : public std::exception {
+            public:
+        };
+
+    class GradeTooLowException : public std::exception {
+            public:
+        };
 
 };
+
+std::ostream& operator<<(std::ostream& os, const Bureaucrat& b);
+// either a Bureaucrat::GradeTooHighException or a Bureaucrat::GradeTooLowException
+
+
 
 #endif
