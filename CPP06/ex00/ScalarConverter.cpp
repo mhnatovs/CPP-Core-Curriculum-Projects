0b9e52c657953df69a01ef1c150ea1bd6ed5f4d6
Marianna Hnatovska <mhnatovs@1-C-12.42heilbronn.de>
2026-06-30 18:59:28 +0200

    ex00

--- CPP06/ex00/ScalarConverter.cpp
+++ CPP06/ex00/ScalarConverter.cpp
@@ -3,6 +3,7 @@
 #include <iomanip>
 #include <cstdlib>
 #include <cmath>
+#include <limits>
 #include <climits>
 #include <cfloat>
 
@@ -38,7 +39,7 @@ static bool isInt(const std::string& data)
 {
     if (data.empty())
         return false;
-    int i = 0;
+    size_t i = 0;
     if (data[i] == '+' || data[i] == '-')
     {
         i++;
@@ -59,7 +60,7 @@ static bool isFloat(const std::string& data)
     if (data.empty())
         return false;
     int point = 0;
-    int i = 0;
+    size_t i = 0;
     bool hasDigit = false;
     if (data[i] == '+' || data[i] == '-')
     {
@@ -94,7 +95,7 @@ static bool isDouble(const std::string& data)
         return false;
     bool hasDigit = false;
     int point = 0;
-    int i = 0;
+    size_t i = 0;
     if (data[i] == '+' || data[i] == '-')
     {
         i++;
@@ -118,9 +119,86 @@ static bool isDouble(const std::string& data)
 
 void ScalarConverter::convert(const std::string& data)
 {
-    int value = std::stoi(data);
-    char c = static_cast<char>(value);
-    int i = static_cast<int>(value);
-    float f = static_cast<float>(value);
-    double d = static_cast<double>(value);
+    char    c = 0;
+    float   f = 0;
+    double  d = 0;
+    int     i = 0;
+    bool    impossible = false;
+
+    if (isChar(data))
+    {
+        c = data[0];
+        i = static_cast<int>(c);
+        f = static_cast<float>(c);
+        d = static_cast<double>(c);
+    }
+    else if (isNanLiteral(data))
+    {
+        d = nan("");
+        f = nanf("");
+        impossible = true;
+    }
+    else if (isInfLiteral(data))
+    {
+        double sign;
+        if (data[0] == '-')
+            sign = -1.0;
+        else
+            sign = 1.0;
+        d = sign * std::numeric_limits<double>::infinity();
+        f = static_cast<float>(d);
+        impossible = true;
+    }
+    else if (isInt(data) || isFloat(data) || isDouble(data))
+    {
+        d = std::strtod(data.c_str(), NULL);
+        f = static_cast<float>(d);
+        if (d > INT_MAX || d < INT_MIN)
+            impossible = true;
+        else
+            i = static_cast<int>(d);
+        c = static_cast<char>(i);
+    }
+    else
+    {
+        std::cout << "Invalid literal." << std::endl;
+        return ;
+    }
+    std::cout << "char: ";
+    if (impossible || d < 0 || d > 127)
+        std::cout << "impossible" << std::endl;
+    else if (i < 32 || i > 126)
+        std::cout << "Non displayable" << std::endl;
+    else
+        std::cout << "'" << c << "'" << std::endl;
+    std::cout << "int: ";
+    if (impossible)
+        std::cout << "impossible" << std::endl;
+    else
+        std::cout << i << std::endl;
+
+    // ---- float ----
+    std::cout << "float: ";
+    if (std::isnan(f))
+        std::cout << "nanf" << std::endl;
+    else if (std::isinf(f))
+        std::cout << (f < 0 ? "-inff" : "+inff") << std::endl;
+    else
+        std::cout << std::fixed << std::setprecision(1) << f << "f" << std::endl;
+
+    // ---- double ----
+    std::cout << "double: ";
+    if (std::isnan(d))
+        std::cout << "nan" << std::endl;
+    else if (std::isinf(d))
+        std::cout << (d < 0 ? "-inf" : "+inf") << std::endl;
+    else
+        std::cout << std::fixed << std::setprecision(1) << d << std::endl;
 }
+
+/*
+
+vj
+g 00419.661
+
+*/
--- CPP06/ex00/main.cpp
+++ CPP06/ex00/main.cpp
@@ -1,56 +1,23 @@
 #include "ScalarConverter.hpp"
 #include <iomanip> 
 #include <iostream>
-
+# include <exception>
+# include <limits>
+# include <sstream>
+# include <string>
 int main(int ac, char**av)
 {
     if (ac != 2)
-        std::cout << "Input correct value\n";
-    
-/*ScalarConverter::convert("42");
-ScalarConverter::convert("a");
-ScalarConverter::convert("4.2f");
-ScalarConverter::convert("nan");
-• char
-• int
-• float
-• double
-спеціальні значення IEEE
-float:
-nanf
-+inff
--inff
-double:
-nan
-+inf
--inf
-ScalarConverter::convert("nan");
-char: impossible
-int: impossible
-float: nanf
-double: nan
-nan не можна перетворити в char або int
-pseudo literals
-static_cast<>
-*/
-
-float f = 114.2f;
-std::cout << f << std::endl;
-int i = static_cast<int>(f);
-std::cout << i << std::endl;
-char c = static_cast<char>(i);
-std::cout << c << std::endl;
-double d = static_cast<double>(f);
-std::cout << d << std::endl;
+    {
+        std::cerr << "Input correct value\n";
+        return (1);
+    }
+    try {
+        ScalarConverter::convert(av[1]);
+    }
+    catch(std::exception &e){
+        std::cerr << "Convertion failed: " << e.what() << std::endl;
+        return (1);
+    }
+    return (0);
 }
-
-// using namespace std;
-
-// int main() {
-//   int num1 = 5;
-//   int num2 = 2;
-//   double sum = static_cast<double>(num1) / num2;
-
-//   cout << fixed << setprecision(1) << sum; // 2.5
-//   return 0;
-// }
